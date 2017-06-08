b4f56ff9fdbc796215f0d9f45245b0051ac75574
hkumar <hkumar@consultant.underarmour.com>
2017-06-08 15:36:08 -0400

    implement swagger for rest api project

--- app.js
+++ app.js
@@ -7,8 +7,29 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+import swaggerJSDoc from 'swagger-jsdoc';
 
 var app = express();
+
+// swagger definition
+const swaggerDefinition = {
+  info: {
+    title: 'Swagger beginner API',
+	version: '1.0.0',
+	description: 'How to use Swagger'
+  },
+	host: 'localhost:3000',
+	basePath: '/'
+};
+// Options for swagger doc
+ const options = {
+ 	swaggerDefinition,
+	 // path to Api Docs
+	 apis: ['./routes/*.js']
+ };
+
+ // Initialize swagger docs
+const swaggerSpec = swaggerJSDoc(options);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +46,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+app.get('/swagger.json', (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(swaggerSpec);
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -42,5 +67,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,120 @@ import express from 'express';
 const router = express.Router();
 import { getAllHobbies, getSingleHobby, createHobby, updateHobby, removeHobby } from '../queries';
 
-/*/!* GET home page. *!/
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});*/
+/**
+ * @swagger
+ * definition:
+ *   Hobby:
+ *     properties:
+ *       name:
+ *         type: string
+ *       type:
+ *         type: string
+ *       isFun:
+ *         type: integer
+ *       Description:
+ *         type: string
+ */
 
+/**
+ * @swagger
+ * /api/hobbies:
+ *   get:
+ *     tags:
+ *       - Hobbies
+ *     description: Returns all hobbies
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of hobbies
+ *         schema:
+ *           $ref: '#/definitions/Hobby'
+ */
 router.get('/api/hobbies', getAllHobbies);
+/**
+ * @swagger
+ * /api/hobbies/{id}:
+ *   get:
+ *     tags:
+ *       - Hobbies
+ *     description: Returns a single hobby
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Hobby's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: A single hobby
+ *         schema:
+ *           $ref: '#/definitions/Hobby'
+ */
 router.get('/api/hobbies/:id', getSingleHobby);
+/**
+ * @swagger
+ * /api/hobbies:
+ *   post:
+ *     tags:
+ *       - Hobbies
+ *     description: Creates a new hobby
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: hobby
+ *         description: Hobby object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Hobby'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
 router.post('/api/hobbies', createHobby);
+/**
+ * @swagger
+ * /api/hobbies/{id}:
+ *   put:
+ *     tags:
+ *     	 - Hobbies
+ *     description: Updates a single hobby
+ *     produces: application/json
+ *     parameters:
+ *       name: hobby
+ *       in: body
+ *       description: Fields for the Hobby resource
+ *       schema:
+ *         type: array
+ *         $ref: '#/definitions/Hobby'
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
 router.put('/api/hobbies/:id', updateHobby);
+/**
+ * @swagger
+ * /api/hobbies/{id}:
+ *   delete:
+ *     tags:
+ *       - Hobbies
+ *     description: Deletes a single hobby
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Hobby's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ */
+
 router.delete('/api/hobbies/:id', removeHobby);
 
 module.exports = router;
